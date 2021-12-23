7a14006bfa5a40c2c3dc56c7c4695db3159bfb61
Fixdii <walera2001@gmail.com>
2021-12-23 20:18:25 +0300

    feat: added form validation

--- src/app/pages/auth-form/auth-form.component.ts
+++ src/app/pages/auth-form/auth-form.component.ts
@@ -9,7 +9,10 @@ import { AuthService } from '../../core/services/auth.service';
   styleUrls: ['./auth-form.component.scss'],
 })
 export class AuthFormComponent implements OnInit {
+  error: string = '';
+
   formGroup!: FormGroup;
+
   @Input() isSignUp: boolean = false;
 
   constructor(
@@ -18,38 +21,73 @@ export class AuthFormComponent implements OnInit {
     private router: Router
   ) {}
 
-  //Validators.pattern('((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30})')
-
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('((?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,30})'),
+        ],
+      ],
     });
+
+    console.log(this.formGroup.get('email'));
   }
 
   submit() {
     const { email, password } = this.formGroup.value;
-
+    this.error = '';
     this.signIn(email, password)
       .then((res) => {
         if (res !== false) {
           this.router.navigate(['']);
         }
       })
-      .catch(err => { console.error(err); });
+      .catch((err) => {
+        this.error = err.message;
+      });
   }
 
   signIn(email: string, password: string) {
-    return this.isSignUp 
+    return this.isSignUp
       ? this.authService.signUp(email, password)
       : this.authService.logIn(email, password);
   }
 
-  loginGoogle(){
-    this.authService.loginWithGoogle();
+  loginGoogle() {
+    this.authService
+      .loginWithGoogle()
+      .then((res) => {
+        if (res !== false) {
+          this.router.navigate(['']);
+        }
+      })
+      .catch((err) => {
+        this.error = err.message;
+      });
   }
 
-  loginFacebook(){
-    this.authService.loginWithFacebook();
+  loginFacebook() {
+    this.authService
+      .loginWithFacebook()
+      .then((res) => {
+        if (res !== false) {
+          this.router.navigate(['']);
+        }
+      })
+      .catch((err) => {
+        this.error = err.message;
+      });
   }
 }
